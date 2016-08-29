2c6c5e908d3db6a2bb22d730fe70900956166e5d
fernandorosero <tatofarp@hotmail.com>
2016-08-29 02:07:38 +0200

    Lenovo i5

--- app/src/main/java/com/thelastmonkey/mycollections/util/MyCollectionConstant.java
+++ app/src/main/java/com/thelastmonkey/mycollections/util/MyCollectionConstant.java
@@ -4,4 +4,8 @@ package com.thelastmonkey.mycollections.util;
  * Created by FERNANDO on 29/08/2016.
  */
 public class MyCollectionConstant {
+    public final String PARAMETRO_ID_COLLECTION = "PARAMETRO_ID_COLLECTION";
+    public final String PARAMETRO_PATH_IMAGEN = "PARAMETRO_PATH_IMAGEN";
+
+
 }
